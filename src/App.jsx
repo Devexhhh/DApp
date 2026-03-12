7f607a8c1abbf56f376018dd9852457fe24720f9
Devexhhh <kumarsujal.codes@gmail.com>
2026-03-12 13:36:04 +0530

    imported SendToken feat

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { WalletModalProvider, WalletDisconnectButton, WalletMultiButton, WalletC
 import "@solana/wallet-adapter-react-ui/styles.css"
 import { Airdrop } from "./Airdrop";
 import { ShowSolBalance } from "./ShowBalance";
+import { SendTokens } from "./SendTokens";
 
 const endpoint = "https://solana-devnet.g.alchemy.com/v2/dhuFXA2gtsJsGgz7diJSo"
 
@@ -15,12 +16,17 @@ const App = () => {
       <ConnectionProvider endpoint={endpoint}>
         <WalletProvider wallets={[]} autoConnect>
           <WalletModalProvider>
-            <div className="w-max-7xl h-full mt-4 flex flex-col gap-2.5 justify-center items-center">
-              <WalletMultiButton />
-              <WalletDisconnectButton />
-              <div className="flex items-center text-2xl">Wallet Adapter For DApp  </div>
-              <Airdrop />
-              <ShowSolBalance />
+            <div className="w-max-7xl h-full mt-4 flex flex-col gap-2.5">
+              <div className="flex flex-row gap-2 px-10">
+                <WalletMultiButton />
+                <WalletDisconnectButton />
+              </div>
+              <div className="flex flex-col gap-4 items-center justify-center">
+                <div className="font-semibold text-2xl font-sans">Wallet adapter for DApp</div>
+                <Airdrop />
+                <ShowSolBalance />
+                <SendTokens />
+              </div>
             </div>
           </WalletModalProvider>
         </WalletProvider>
