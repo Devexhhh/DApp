8118c3f3647946fa280d218b745bebdbb7542c33
Devexhhh <kumarsujal.codes@gmail.com>
2026-03-12 13:01:51 +0530

    added Airdrop component

---
+++ src/Airdrop.jsx
@@ -0,0 +1,20 @@
+import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+
+export const Airdrop = () => {
+    const wallet = useWallet();
+    const { connection } = useConnection();
+
+    const sendAirdropToUser = async () => {
+        await connection.requestAirdrop(wallet.publicKey, 1000000000)
+        alert("airdropped sol")
+    }
+
+    return (
+        <>
+            <div className="flex flex-row gap-2">
+                <input type="text" placeholder="Amount" className="border rounded-sm px-3 py-1"></input>
+                <button onClick={sendAirdropToUser} className="cursor-pointer border px-3 py-1 rounded-sm bg-violet-500 text-white">Send Airdrop</button>
+            </div>
+        </>
+    )
+}
