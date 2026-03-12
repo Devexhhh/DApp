f2219573992c1662185d1f1bff5255981d6d0fd1
Devexhhh <kumarsujal.codes@gmail.com>
2026-03-12 13:01:41 +0530

    added Wallet Providers

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { FC } from "react";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { WalletModalProvider, WalletDisconnectButton, WalletMultiButton, WalletConnectButton } from "@solana/wallet-adapter-react-ui";
+import "@solana/wallet-adapter-react-ui/styles.css"
+import { Airdrop } from "./Airdrop";
+
+const endpoint = "https://solana-devnet.g.alchemy.com/v2/dhuFXA2gtsJsGgz7diJSo"
+
+const App = () => {
+  return (
+    <>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={[]} autoConnect>
+          <WalletModalProvider>
+            <div className="w-max-7xl h-full mt-4 flex flex-col gap-2.5 justify-center items-center">
+              <WalletMultiButton />
+              <WalletDisconnectButton />
+              <div className="flex items-center text-2xl">Wallet Adapter For DApp  </div>
+              <Airdrop />
+            </div>
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </>
+  )
+}
+
+export default App
